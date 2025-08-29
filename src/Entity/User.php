e913035b948112d420c8abe174a23114fc6278df
Sami <sami.ousmaal@supdevinci-edu.fr>
2025-08-29 16:40:02 +0200

    Update database connection settings and add User entity and repository

---
+++ src/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+class User
+{
+
+    private $id;
+
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
